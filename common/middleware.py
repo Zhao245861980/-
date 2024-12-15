233104a998300e01c8c1089d1356d6179e5fc42c
Zhao245861980 <245861980@qq.com>
2024-12-16 03:01:34 +0800

    '完成了获取用户资料，添加了登陆验证中间键'

--- common/middleware.py
+++ common/middleware.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
+from user.models import User
+from lib.http import render_json
+from common import error
+
+
+class AuthMiddleware(MiddlewareMixin):
+    """用户登录中间件"""
+    def process_request(self, request):
+        uid = request.session.get('uid')
+        if uid:
+            try:
+                request.user = User.objects.get(id=uid)
+                return
+            except User.DoesNotExist:
+                request.session.flush()
+        return render_json(None, code=error.LOGIN_ERROR)
+
 
 class CorMiddleware(MiddlewareMixin):
     """处理客 JS 户端的跨域"""
